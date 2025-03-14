c890f794ffff25fa4942c6137f2c555ed63906f9
gn0d3z04m <m40_z3d0ng@kali.kali>
2025-03-14 16:16:33 +0300

    add comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(){
-    std::string name;
+    std::string name; //comment
     std::cout << "Enter name: ";
     std::cin >> name;
     std::cout << "Hello, world from " << name << std::endl;
