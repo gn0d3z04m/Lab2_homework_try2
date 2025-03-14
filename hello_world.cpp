031c33a63cbc741626f739dae71e2d4140010ff5
gn0d3z04m <styrelwho@gmail.com>
2025-03-14 16:27:25 +0300

    Update comment hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(){
-    std::string name; //comment
+    std::string name; //com_mm_ment
     std::cout << "Enter name: ";
     std::cin >> name;
     std::cout << "Hello, world from " << name << std::endl;
